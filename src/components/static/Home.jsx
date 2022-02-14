de8737161ef55899f20e54b087c949ca8b403cd3
Nicole Steadman <nsteadman@gmail.com>
2022-02-14 12:06:07 -0800

    made some text changes to Home component

--- src/components/static/Home.jsx
+++ src/components/static/Home.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Home = () => {
   return <div>
       <h1>Welcome to Album Tracker!</h1>
-      <h3>Get started adding new albums by clicking CREATE ALBUMS!</h3>
-      <h3>View your album collection by clicking LIST ALBUMS!</h3>
+      <h4>Get started adding new albums by clicking CREATE ALBUMS!</h4>
+      <h4>View your album collection by clicking LIST ALBUMS!</h4>
   </div>;
 };
 
