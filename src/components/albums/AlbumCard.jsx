1546f13d58645970e7c07de3b183463ef0472209
Nicole Steadman <nsteadman@gmail.com>
2022-02-22 09:38:36 -0800

    Added upvote/downvote function

--- src/components/albums/AlbumCard.jsx
+++ src/components/albums/AlbumCard.jsx
@@ -3,15 +3,31 @@ import AlbumInfo from "./AlbumInfo";
 
 const AlbumCard = ({ album }) => {
   const [visibleDetails, setVisibleDetails] = useState(false);
+  const [counter, setCounter] = useState(0);
 
   const handleClick = () => {
     setVisibleDetails(!visibleDetails)
+  }
+
+  const increment = () => {
+    setCounter(counter + 1)
+  }
+
+  const decrement = () => {
+    setCounter(counter - 1)
   }
 
   return (
   <div key={album.id} id="album-info">
       <img onClick={handleClick} src={album.image} alt={album.album} />
       { visibleDetails ? <AlbumInfo album={album} /> : ""}
+      <button onClick={increment}>
+      👍
+      </button>
+      {counter}
+      <button onClick={decrement}>
+      👎
+      </button>
   </div>
   )
 };
