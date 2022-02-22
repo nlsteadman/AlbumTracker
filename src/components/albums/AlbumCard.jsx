cc40d6c7399900e2beb55d7bbb410a221330ba77
Nicole Steadman <nsteadman@gmail.com>
2022-02-22 09:13:38 -0800

    Added favorite function

--- src/components/albums/AlbumCard.jsx
+++ src/components/albums/AlbumCard.jsx
@@ -3,6 +3,7 @@ import AlbumInfo from "./AlbumInfo";
 
 const AlbumCard = ({ album }) => {
   const [visibleDetails, setVisibleDetails] = useState(false);
+  const [isFavorited, setIsFavorited] = useState(false);
 
   const handleClick = () => {
     setVisibleDetails(!visibleDetails)
@@ -12,6 +13,21 @@ const AlbumCard = ({ album }) => {
   <div key={album.id} id="album-info">
       <img onClick={handleClick} src={album.image} alt={album.album} />
       { visibleDetails ? <AlbumInfo album={album} /> : ""}
+      <div>
+        {isFavorited ? (
+          <button
+            onClick={() => setIsFavorited(false)}
+          >
+            ❤️
+          </button>
+        ) : (
+          <button
+            onClick={() => setIsFavorited(true)}
+          >
+            ♡
+          </button>
+        )}
+      </div>
   </div>
   )
 };
